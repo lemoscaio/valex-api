2308bc1a11d9b7a22d940ef574017f996bd70072
Caio Lemos <caio_lemos94@hotmail.com>
2022-07-09 10:14:07 -0300

    refactor: removes magic numbers from formatEmployeeName and creates a .env.example

--- utils/formatEmployeeName.ts
+++ utils/formatEmployeeName.ts
@@ -1,15 +1,21 @@
 export function formatEmployeeName(fullName: string) {
   const namesInArray: string[] = fullName.split(" ")
 
+  const nameLengthLimit = 3
+
   const formattedName = namesInArray.reduce(
     (acc: string, currentName: string, currentIndex: number) => {
-      if (currentIndex === 0 || currentIndex === namesInArray.length - 1)
-        return `${acc} ${currentName}`
-      if (currentName.length >= 3) return `${acc} ${currentName[0]}`
+      if (
+        currentName.length >= nameLengthLimit &&
+        currentIndex !== 0 &&
+        currentIndex !== namesInArray.length - 1
+      )
+        return `${acc} ${currentName[0]}`
       else return `${acc} ${currentName}`
     },
     "",
   )
 
+  console.log(formattedName)
   return formattedName.trim()
 }
