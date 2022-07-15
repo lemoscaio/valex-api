bc6850aeb0487236c369e50f7108b1b925088f8b
lemoscaio <caio_lemos94@hotmail.com>
2022-07-15 02:55:23 -0300

    fix: add regex to allow only numbers on password

--- schemas/activateCardSchema.ts
+++ schemas/activateCardSchema.ts
@@ -3,5 +3,8 @@ import Joi from "joi"
 export const activateCardSchema = Joi.object({
   cardId: Joi.number().required(),
   securityCode: Joi.string().length(3).required(),
-  password: Joi.string().length(4).required(),
+  password: Joi.string()
+    .length(4)
+    .pattern(/[\d]{4}/)
+    .required(),
 })
